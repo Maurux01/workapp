89628dbe8b0c1cd01e2397bf6cd79eee6aa0757e
maurux01 <1w6zu7edc@mozmail.com>
2026-09-15 13:05:39 -0500

    generation treeview, empty docs for now, starting with making the window for t the app

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-#Code Totally made by Maurux01
-#dependcies imported
+# Code Totally made by Maurux01
+# dependcies imported
+# █   █  ███  █   █ ████  █   █ █   █  ███    █
+# ██ ██ █   █ █   █ █   █ █   █  █ █  █   █  ██
+# █ █ █ █████ █   █ ████  █   █   █   █   █   █
+# █   █ █   █ █   █ █  █  █   █  █ █  █   █   █
+# █   █ █   █  ███  █   █  ███  █   █  ███   ███
+
+
 import customtkinter as ctk
 import webbrowser
 import threading
+import flet as fl
 from searcher import search_jobs
 
 
-#global config
+# global config
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
-#definitions
+# definitions
 self = str
-
-
-
-class workapp(ctk.CTk):
-    def_init_(self);
-    super().__init__()
-
-    #Sup frame
-    self.title("🎯 Workapp- work searcher")
-        self.geometry("900x700")
-        self.minsize(700, 500)
-
-    top_frame = ctk.CTkFrame(self)
-    top_frame= 
